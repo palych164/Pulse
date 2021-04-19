44be5cb44ddd058423af8ca58999b7b3da487d9c
palych164 <palych164@gmail.com>
2021-04-19 13:38:01 -0400

    no sm adapt

---
+++ src/js/scrypt.js
@@ -0,0 +1,73 @@
+// var name = 'evgeny';
+
+// let number = 7;
+// const pi = 3.14;
+
+// number = 4;
+
+// let leftBorderWidth = 200;
+
+//number
+//string   строка словами  '', "", ``
+// true/false
+// null 
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+// Symbol
+
+// alert(1234)
+// console.log(number)
+// let answer = confirm('Вам есть 18?')
+// console.log(answer);
+
+// let answer = prompt('Вам есть 18?')
+// console.log(answer);
+
+// console.log(4 + "sdf");
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if (2*1 == 8*1) {
+//     console.log('верно')
+// } else {
+//     console.log('ошибка')
+// }
+
+// let answer = confirm('Вам есть 18?')
+// if  (answer) {
+//     console.log('go in')
+// } else{
+//     console.log('bye')
+// }
+
+// const num = 48;
+
+// if (num < 49) {
+//     console.log('no')
+// } else if (num>100){
+//     console.log('many')
+// } else {
+//     console.log('good')
+// }
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+// function logging(a, b){
+//     console.log(a + b)
+// }
+
+// logging(3, 5);
+
+
+// logging(6, 4);
