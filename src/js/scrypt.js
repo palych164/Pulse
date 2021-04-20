02ddce5d6c3fc179bf4152b019f09af39bf2f574
palych164 <palych164@gmail.com>
2021-04-20 18:41:40 -0400

    with catalog + adapt

--- src/js/scrypt.js
+++ src/js/scrypt.js
@@ -80,12 +80,12 @@
 // $(document).ready(function(){
 //     $('.carousel__inner').slick({
 //         speed: 1200,
-//         // adaptiveHeight: true,
+//         adaptiveHeight: true,
 //         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"></button>',
 //         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"></button>',
 //         responsive: [
 //             {
-//                 breakpoint: 768,
+//                 breakpoint: 992,
 //                 settings: {
 //                   dots: true,
 //                   arrows: false
@@ -97,26 +97,48 @@
 
 //  "2  способ через Tiny slider"
 
+
+// main slider Tiny Slider
 const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    controls: false,
-    nav: false,
-    responsive: {
-        640: {
-          edgePadding: 20,
-          gutter: 20,
-          items: 1
-        },
-    }
-  });
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-  })
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  })
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  controls: false,
+  nav: false
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+
+
+// Tabs jQuery scrypt
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+});
+
+    
+
 
 
 
@@ -141,4 +163,4 @@ const slider = tns({
 //   });
   
 
-// 3 способ Bootsrap
+// 3 способ
